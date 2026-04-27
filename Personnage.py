22ae22181fbb5a7881ea3cf998f0584180e51dcc
Raph059 <113098925+Raph059@users.noreply.github.com>
2026-04-27 10:35:47 +0200

    Ajout commentaires

--- Personnage.py
+++ Personnage.py
@@ -2,6 +2,7 @@ import pygame
 
 class Personnage:
     def __init__(self, vie, vie_max, vitesse, x_depart, y_depart):
+        """Fonction d'initialisation des statistiques basiques du personnage : sa vie, sa vitesse et ses coordonnées 2D (x et y)"""
         self.vie = vie
         self.vie_max = vie_max
         self.vitesse = vitesse
@@ -11,9 +12,10 @@ class Personnage:
         self.temps_ko = 0
 
     def deplacement(self, dx, dy, labyrinthe):
+        """Fonction qui permet de faire déplacer le personnage en modifiant ses variables x et y"""
         if self.ko:
             return False
-        
+        else :
         nouveau_x = self.x + dx
         nouveau_y = self.y + dy
 
@@ -27,10 +29,11 @@ class Personnage:
                 if self.vie <= 0:
                     self.ko = True
                     self.temps_ko = pygame.time.get_ticks()
-                return True # Signal de collision pour l'effet rouge
+                return True
         return False
 
     def update_ko(self):
+        """Fonction qui enlève l'état KO après 3 secondes."""
         if self.ko and pygame.time.get_ticks() - self.temps_ko >= 3000:
             self.ko = False
             self.vie = self.vie_max
