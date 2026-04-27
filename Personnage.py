05493540fb499c8eb6f85f6a2d436e21e5757161
Raph059 <113098925+Raph059@users.noreply.github.com>
2026-04-27 10:25:57 +0200

    Modification Personnage.py
    Modification de Personnage permettant la liaison avec la matrice

--- Personnage.py
+++ Personnage.py
@@ -1,36 +1,36 @@
 import pygame
 
 class Personnage:
-    def __init__(self, vie, vie_max, vitesse):
-        """Fonction d'initialisation des statistiques basiques du personnage : sa vie, sa vitesse et ses coordonnées 2D (x et y)."""
+    def __init__(self, vie, vie_max, vitesse, x_depart, y_depart):
         self.vie = vie
-        self.vie_max = vie
+        self.vie_max = vie_max
         self.vitesse = vitesse
-        self.x = 0
-        self.y = 0
+        self.x = x_depart
+        self.y = y_depart
         self.ko = False
         self.temps_ko = 0
 
     def deplacement(self, dx, dy, labyrinthe):
-        """Fonction qui permet de faire déplacer le personnage en modifiant ses variables x et y."""
-        if self.ko:   # Si le personnage est KO, il ne bouge pas
-            return
-        else:
-            nouveau_x = self.x + dx
-            nouveau_y = self.y + dy
+        if self.ko:
+            return False
+        
+        nouveau_x = self.x + dx
+        nouveau_y = self.y + dy
 
-            if labyrinthe[nouveau_y][nouveau_x] != 1:   # Si pas de mur, il se déplace
+        if 0 <= nouveau_x < len(labyrinthe[0]) and 0 <= nouveau_y < len(labyrinthe):
+            if labyrinthe[nouveau_y][nouveau_x] != 1:
                 self.x = nouveau_x
                 self.y = nouveau_y
+                return False
             else:
-                self.vie -= 20   # Sinon il perd 20 PV
-                # ajout tremblement cam
-                if self.vie <= 0: # Si sa vie passe sous la barre des 0
+                self.vie -= 20
+                if self.vie <= 0:
                     self.ko = True
-                    self.temps_ko = pygame.time.get_ticks() #Initialisation de la variable temps_ko qui enregistre le moment de la collisions
+                    self.temps_ko = pygame.time.get_ticks()
+                return True # Signal de collision pour l'effet rouge
+        return False
 
     def update_ko(self):
-        """Fonction qui enlève l'état KO après 3 secondes."""
         if self.ko and pygame.time.get_ticks() - self.temps_ko >= 3000:
             self.ko = False
             self.vie = self.vie_max
